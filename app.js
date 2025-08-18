9ee6fddb5648f60ea2429e81a5b414e24d339635
quentinlang <anima529@hotmail.com>
2025-08-18 22:09:26 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const socket = io("http://localhost:4001");
+const socket = io("http://camlive-backend.onrender.com");
 const pc = new RTCPeerConnection();
 const localVideo = document.getElementById("localVideo");
 const remoteVideo = document.getElementById("remoteVideo");
